8631850ec16c2dd882839dc9be68a752d78ea9ac
albertferreira <albert.ferreira@vileve>
2021-05-12 12:41:01 -0300

    Include template in react

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ function App() {
           <Route path="/about" exact component={() => <About />} />
           <Route path="/contact" exact component={() => <Contact />} />
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     </div>
   );
