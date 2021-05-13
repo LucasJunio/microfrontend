e80b972be23aa915d983ce9d3570d9a02e7d0546
albertferreira <albert.ferreira@vileve>
2021-05-13 16:52:27 -0300

    New Template empty with react

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,61 +1,42 @@
 import React from 'react'
-import {Classlogotipo} from './styles'
+import logo from '../../assets/images/logo-vileve-pay-cor-140px.png'
+import Background from '../../assets/images/bg2.jpg'
 
-const home =() => {
-   
+import { Form, Input, Card, CardHeader, CardBody } from 'reactstrap';
+
+import {
+   Classlogotipo
+  ,ClassBackground
+  ,TitleWelcome
+  ,ContainerCard
+} from './styles'
+
+const home =() => {   
     return (
      <>      
-      <Classlogotipo><img src="images/logo-vileve-pay-cor-140px.png" alt="logotipo"></img></Classlogotipo> 
-      
-      
-      <section class="ftco-section">
-      <div class="container">
-      <div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Gateway de Pagamentos Vileve</h2>
-				</div>
-			</div>
+      <Classlogotipo><img src={logo} alt="logotipo"></img></Classlogotipo>
 
-			<div class="row justify-content-center">
-				<div class="col-md-6 col-lg-4">
-					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Realize seu cadastro</h3>
-      
-            <form action="#" class="signin-form" onsubmit="criptpassword()">
-            <div class="form-group">
-		      	<input type="text" id="nome" name="nome" class="form-control" placeholder="Nome Completo" required oninvalid="this.setCustomValidity('O campo Nome Completo é obrigatório!')" oninput="setCustomValidity('')" />
-						<input type="text" id="celular" name="celular" class="form-control" placeholder="Celular" required />
-						<input type="email" class="form-control" placeholder="E-mail" required oninvalid="this.setCustomValidity('O campo E-mail também é obrigatório!')" oninput="setCustomValidity('')" />
-		        </div>
+      <TitleWelcome>Gateway de Pagamentos Vileve</TitleWelcome>
+      <ContainerCard>
 
-            <div class="form-group">
-	              <input id="senha" name="senha" type="password" class="form-control" placeholder="Password" required oninvalid="this.setCustomValidity('Insira uma senha!')" oninput="setCustomValidity('')" />
-	              <span toggle="#senha" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-	            </div>
-              
-              <div class="form-group d-md-flex">
-              <div class="w-100">
+      <Card>
+      <CardHeader>Cadastro</CardHeader>
+      <CardBody>
 
-              <label class="checkbox-wrap checkbox-secondary">Aceito os termos Vileve
-									  <input type="checkbox" checked />
-									  <span class="checkmark"></span>
-									</label>
-
-              </div>
-              </div>
-
-          <div class="form-group">
-					<button type="submit" class="form-control btn btn-primary submit px-3">Crie sua conta</button>
-				  </div>
-
-            </form>
-      
-      
-      </div></div></div>
+      <Form>
+      <Input id='nome' name='nome' placeholder="Nome Completo" />
+      <Input id='email' name='email' placeholder="E-mail" />
+       
+      </Form>
+      </CardBody>
+      </Card>
 
 
-      </div>
-      </section>
+      </ContainerCard>
+
+    <ClassBackground style={{backgroundImage:`url(${Background})`}}></ClassBackground>
+
+
      </>
     );
   }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,6 +1,43 @@
 import styled from 'styled-components'
 
 export const Classlogotipo = styled.div`
-    position: absolute;
-    margin:30px
-`;
+position: absolute;
+margin:30px;
+z-index:2
+`
+
+export const TitleWelcome = styled.div`
+position: absolute;
+text-align:center;
+align-items:center;
+color:#fff;
+font-size:25px;
+margin-top:5%;
+text-shadow: 0 0 3px #ccc;
+width:100%;
+z-index:2
+`
+
+
+
+export const ContainerCard = styled.div`
+left:33%;
+position: absolute;
+max-width:30%;
+min-width:35%;
+margin-top:10%;
+z-index:10
+`
+
+
+export const ClassBackground = styled.div`
+position:absolute;
+width:100%;
+height:100%;
+background-attachment: fixed;
+ background-repeat: no-repeat;
+ background-size: cover;
+ -moz-background-size: cover;
+ overflow-x: hidden;
+ overflow-y: hidden;
+`
