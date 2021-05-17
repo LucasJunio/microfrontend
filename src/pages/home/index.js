d4534749e62453c27893e35ca1c3eb93a2a55fd5
albertferreira <albert.ferreira@vileve>
2021-05-17 17:09:54 -0300

    Create page Home after signup #382

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,268 +1,189 @@
-import React from 'react'
-import $ from "jquery";
-import axios from "axios"
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-import logo from '../../assets/images/logo-vileve-pay-cor-140px.png'
+const drawerWidth = 240;
 
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Checkbox from "@material-ui/core/Checkbox";
-import Check from "@material-ui/icons/Check";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Icon from "@material-ui/core/Icon";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
 
-// import Header from "components/Header/Header.js";
-// import HeaderLinks from "components/Header/HeaderLinks.js";
-import Footer from "components/Footer/Footer.js";
-import Button from "components/CustomButtons/Button.js";
-import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-// import CardHeader from "components/Card/CardHeader.js";
-// import CardFooter from "components/Card/CardFooter.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
-
-import Email from '@material-ui/icons/EmailOutlined';
-import People from "@material-ui/icons/PeopleAltOutlined";
-import Phone from "@material-ui/icons/PhoneIphone"
- 
-// import { Form, Input, Card, CardBody } from 'reactstrap';
-
-import {
-   Classlogotipo
-  ,ClassBackground
-  ,TitleWelcome
-  ,ContainerCard
-  ,ContainerCardSize
-  ,PositionButton
-  ,PositionFooter
-  ,DescriptionText
-} from './styles'
-
-import styles from "assets/jss/material-kit-react/views/loginPage.js";
-import { func } from 'prop-types';
-const useStyles = makeStyles(styles);
-
-
-const home =() => { 
-
+export default function MiniDrawer() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    const [checked, setChecked] = React.useState('false');
-    const handleToggle = value => {
-    checked.indexOf(value) === -1 ? setChecked('true'):setChecked('false')
-    }
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
- 
-    const [Nome, setNome] = React.useState(''); 
-    const OnchangeNome = v =>{
-    setNome(v);
-    (/^[A-Za-z-ç]+$/).test(v) || v.length < 1 ? $('#descriptionnome').html('') : $('#descriptionnome').html('Digite apenas letras no campo nome!')
-    }
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    const [email, setEmail] = React.useState(''); 
-    const OnchangeEmail = v =>{
-    setEmail(v);
-    (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/).test(v) || v.length < 1 ? $('#descriptionemail').html('') : $('#descriptionemail').html('Digite um email válido')
-    }
- 
-    const [Celular, setCelular] = React.useState(''); 
-    const OnchangeCelular = v =>{
-      function maskcel(v){
-        v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
-        v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
-        v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
-        return v;
-      }
-    setCelular(maskcel(v))
-    }
-
-    const [senha, setSenha] = React.useState(''); 
-    const [Iconsenha, setIconSenha] = React.useState('lock_Outline'); 
-    const [ColorInputClass, setColorInputClass] = React.useState(false); 
-    const OnchangeSenha = v =>{
-    setSenha(v);
-    if((/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/).test(v)){
-    setIconSenha('check_Outline');setColorInputClass(true);$('#descriptionpassword').html('')
-    }else{setIconSenha('lock_Outline');setColorInputClass(false);$('#descriptionpassword').html(`A senha deve conter mínimo de oito caracteres, <br> pelo menos, uma letra maiúscula, uma letra minúscula, <br> números e um caractere especial`)}
-    }
-
-    const Register =()=>{
- 
-    if(Nome.length < 5 || !(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/).test(email) || Celular.length != 15 || !(/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/).test(senha)){
-      alert('Todos os campos são obrigatórios, favor revise seu formulário!')
-    }else{
-       checked ==='false' ? alert('Favor leira os termos e clique em confirmar!') : submit()
-    }
-
-    function submit(){
-      
-      axios({
-        method: 'post',
-        url: 'http://',
-        data: {
-          nome: Nome,
-          email: email,
-          celular:Celular,
-          senha:senha
-        }
-      }).then(function (response) {
-        alert(response) 
-      }).catch(function (error) {
-        console.log(error);
-      });
-
-    }
-
-  }
-    return (
-     <>      
-      <Classlogotipo><img src={logo} alt="logotipo"></img></Classlogotipo> 
-
-      <TitleWelcome>Gateway de Pagamentos Vileve</TitleWelcome>
-
-
-      <ContainerCard>
-      <ContainerCardSize>
-      <Card>        
-
-            <CardBody>
-
-              <CustomInput
-                labelText="Nome Completo"
-                id="nome"
-                name="nome"
-                formControlProps={{
-                  fullWidth: true,
-                }}
-                inputProps={{
-                  type: "text",
-                  onChange: (e) => OnchangeNome(e.target.value),
-                  autoComplete: "off",
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <People className={classes.inputIconsColor} />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <DescriptionText><div id="descriptionnome"></div></DescriptionText>
-
-
-              <CustomInput
-                labelText="E-mail"
-                id="email"
-                name="email"
-                formControlProps={{
-                  fullWidth: true,
-                }}
-                inputProps={{
-                  type: "email",
-                  onChange: (e) => OnchangeEmail(e.target.value),
-                  autoComplete: "off",
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Email className={classes.inputIconsColor} />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <DescriptionText><div id="descriptionemail"></div></DescriptionText>
-
-
-              <CustomInput
-                labelText="Celular"
-                id="celular"
-                name="celular"
-                formControlProps={{
-                  fullWidth: true,
-                }}
-                inputProps={{
-                  type: "text",
-                  onChange: (e) => OnchangeCelular(e.target.value),
-                  value: Celular,
-                  autoComplete: "off",
-                  inputProps: { maxLength: 15 },
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Phone className={classes.inputIconsColor} />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <DescriptionText><div id="descriptioncelular"></div></DescriptionText>
-               
-              <CustomInput
-                labelText="Senha"
-                id="password"
-                name="password"
-                success={ColorInputClass}
-                formControlProps={{
-                  fullWidth: true,
-                }}
-                inputProps={{
-                  type: "password",
-                  onChange: (e) => OnchangeSenha(e.target.value),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                    <Icon className={classes.inputIconsColor}>{Iconsenha}</Icon>                      
-                    </InputAdornment>
-                  ),
-                  autoComplete: "off",
-                }}
-              />
-               
-            <DescriptionText>
-            <div id="descriptionpassword">A senha deve conter mínimo de oito caracteres, <br></br> pelo menos, uma letra maiúscula, uma letra minúscula, <br></br> um número e um caractere especial </div>
-            </DescriptionText>
-
-        <div className={classes.checkboxAndRadio + " " + classes.checkboxAndRadioHorizontal}/>
-        <FormControlLabel
-          control={
-            <Checkbox
-              tabIndex={-1}
-              onClick={() => handleToggle('true')}
-              checkedIcon={<Check className={classes.checkedIcon} />}
-              icon={<Check className={classes.uncheckedIcon} />}
-              classes={{
-                checked: classes.checked,
-                root: classes.checkRoot
-              }}
-            />
-          }
-          classes={{ label: classes.label }}
-          label="Concordo com os termos Vileve"
-        />
-     
-
-              <PositionButton>
-              <Button
-                // simple
-                color="primary"
-                size="lg"
-                // href="#"
-                // target="_blank"
-                rel="noopener noreferrer"
-                onClick={() => Register()}
-              >
-                Crie sua Conta
-              </Button>
-              </PositionButton>
-
-                </CardBody>
-
-      </Card>
-      </ContainerCardSize>
-      </ContainerCard>
-
-      <ClassBackground ></ClassBackground>
-        
-      <PositionFooter><Footer/></PositionFooter>
-      
-
-
-     </>
-    );
-  }
-  
-  export default home;
-  
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Bem Vindo ao Gateway de Pagamentos Vileve
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
 import Home from '../pages/home'
 import Login from '../pages/login'
+import Sigup from '../pages/signup'
+
 
 const Routes =() => {
   return (
    <Router>
      <Switch>
      <Route exact path='/'>
-      <Home/>
+      <Sigup/>
      </Route>
      <Route path='/login'>
       <Login/>
+     </Route>  
+     <Route path='/home'>
+      <Home/>
      </Route>  
      </Switch>  
    </Router>
