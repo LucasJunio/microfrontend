a8a498b5a7e15a7a7e930f66e92f85dfb0fbade1
albertferreira <albert.ferreira@vileve>
2021-05-18 10:19:30 -0300

    create modal and home page #384

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -18,6 +18,23 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+
+ 
+
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -95,7 +112,43 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+  const [openmodal, setOpenmodal] = React.useState(true);
+  const handleClickOpen = () => {
+    setOpenmodal(true);
+  };
+  const handleClose = () => {
+    setOpenmodal(false);
+  };
+
   return (
+
+    <>
+
+      <Dialog open={openmodal} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Informe o Token enviado por SMS</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Informe o token enviado para seu celular para que confirme sua conta no gateway de pagamentos Vileve.
+          </DialogContentText>
+
+          <TextField
+            autoFocus
+            // margin="dense"
+            id="name"
+            label="Token"
+            type="email"
+            autoComplete="false"
+            // fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Enviar
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+    
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
@@ -121,6 +174,7 @@ export default function MiniDrawer() {
           </Typography>
         </Toolbar>
       </AppBar>
+
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
@@ -161,29 +215,12 @@ export default function MiniDrawer() {
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
+        <h3>Gateway de Pagamentos Vileve</h3>
+          Seja bem vindo à área de administração do seu gateway de pagamentos vileve.
         </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+ 
       </main>
     </div>
+    </>
   );
 }
