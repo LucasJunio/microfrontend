c74394402ebb404fc5f7289b32668e4b4403f856
albertferreira <albert.ferreira@vileve>
2021-06-01 11:54:21 -0300

    Finish front react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
-
 import "assets/scss/material-kit-react.scss?v=1.10.0";
-
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
