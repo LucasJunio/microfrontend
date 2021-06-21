e59a5ce1d837985f0f92c3d15ed625433c11836f
albertferreira <albert.ferreira@vileve>
2021-06-21 14:37:12 -0300

    inlude email

---
+++ src/pages/email/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {
+ ClassBackground
+} from './styles'
+
+const Email =() => {
+    return (
+     <>
+     
+     <ClassBackground></ClassBackground>
+     </>
+    );
+  }
+export default Email;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 import Home from '../pages/home'
 import Login from '../pages/login'
 import Signup from '../pages/signup'
 import Products from '../pages/products'
+import Email from '../pages/email'
 
 import PrivateRoute from './privateroute'
 
@@ -14,6 +16,7 @@ const Routes =() => {
      <Route exact path='/'><Signup/></Route>
      <Route path='/login'><Login/></Route>
      <Route path='/products'><Products/></Route>    
+     <Route path='/email'><Email/></Route> 
      <PrivateRoute path='/home'><Home/></PrivateRoute>  
      </Switch>  
    </Router>
