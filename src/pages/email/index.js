4be9e1451cfcbc62a087252742c287c8a76402de
albertferreira <albert.ferreira@vileve>
2021-07-14 13:49:17 -0300

    include ajax email call

--- src/pages/email/index.js
+++ src/pages/email/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import $ from "jquery";
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -17,12 +17,45 @@ import { useDispatch, useSelector } from 'react-redux';
 import { signupRequest, signupSuccess } from '../../store/modules/signup/actions';
 import history from '../../services/history';
 
+const getUrlParameter = (sParam) => {
+  var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('?'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+  }
+  return false;
+};
+
 const Email =() => {
   const dispatch = useDispatch();
-  var modal = React.useState(true);
+  const [modal, setOpenmodal] = React.useState(false);
+
+
+  $.ajax({
+    url: `http://3.233.0.255:3001/validation/email/${getUrlParameter('token')}`,
+    type: 'GET',
+    crossDomain: true,
+    cache: false,
+    success: (result) => {
+    dispatch(signupSuccess()); 
+    setOpenmodal(true);
+    $('#form-dialog-body').html(`<b>Seja bem vindo à Vileve,</b> seu <b>email foi confirmado</b>, agora você está pronto pra começar!.`);
+    },
+    error: (error) => {
+       setOpenmodal(true);
+       $('#form-dialog-body').html(`<b><span style="color:red">Erro :(</span></b> ${JSON.stringify(error.responseJSON.message.message)}.`);
+      }
+  })
 
   const handleClose = () => {
-    dispatch(signupSuccess());
+    // dispatch(signupSuccess());
     history.push('/');
   };
 
@@ -32,8 +65,8 @@ const Email =() => {
         <Dialog open={modal} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title"><ThumbUpAltOutlinedIcon style={{ marginTop: 15, marginRight: 5 }}/>  Seja Bem Vindo ao Grupo Vileve</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            <b>Seja bem vindo à Vileve,</b> seu <b>email foi confirmado</b>, agora você está pronto pra começar!.
+          <DialogContentText id="form-dialog-body">
+            
           </DialogContentText>
         </DialogContent>
         <DialogActions>
