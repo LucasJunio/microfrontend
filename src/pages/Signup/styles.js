613e5e45724cd94faf55815563e2d4e22aafca62
carlosx71 <carlos.x71@hotmail.com>
2021-09-30 23:16:48 -0300

    change repeat in singup

--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
   container: {
     backgroundImage: `url(${Background})`,
+    backgroundRepeat: 'repeat-y',
     width: '100%',
     height: '100%',
     position: 'absolute',
