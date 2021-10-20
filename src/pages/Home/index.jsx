e3ffe6eca9a895bb42fdde3ccbfeb57099824f58
LucasJunio <lucas.verdine@gmail.com>
2021-10-20 17:05:37 -0300

    feat: :construction: lju_964

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -17,10 +17,12 @@ import {
   InputBase,
   Grid,
   Typography, 
-  Container
+  Container, 
+  Card, CardContent
 } from "@material-ui/core";
 import { PhoneIphone, Edit } from "@material-ui/icons";
 import { VictoryChart, VictoryBar, VictoryTheme, VictoryLabel,  VictoryAnimation, VictoryLine, VictoryPie } from 'victory';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useDispatch, useSelector } from "react-redux";
 import ButtonTimer from "../../components/ButtonTimer";
 import { validationStatus } from "../../store/ducks/Validation";
@@ -127,60 +129,127 @@ export default function Dashboard() {
       : setOpenModal(true);
   };
 
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  
+
   return (
     <Page>
       <Backdrop className={classes.backdrop} open={openBackDrop}>
         <CircularProgress color="inherit" />
       </Backdrop>
 
-      <Grid container direction='column' style={{ backgroundColor: "#eaeef3" }} spacing={2} alignItems="center" >
+      <Grid container direction='column' style={{ backgroundColor: "#F6F6F6" }} spacing={2} alignItems="center" >
         <Grid item>
-          <Grid container direction='row' spacing={4}>
-            <Grid item xs={12} lg={6}>
-              <VictoryChart
-                theme={VictoryTheme.material}
-                domainPadding={{ x: 10 }}
-                style={{
-                  fill: "hsl(355, 20%, 90%)",
-                  fontSize: 11
-                }}
-                height={250}
-                horizontal
-              >
-                <VictoryBar
-                  barRatio={0.35}
-                  style={{
-                    data: { fill: "#005882" }                    
-                  }}
-                  data={chartTransactedAmount}
-                />
-              </VictoryChart>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-              <VictoryChart
-                  theme={VictoryTheme.material}
-                  domainPadding={{ x: 15 }}
-                  height={250}
-                >
-                  <VictoryLine
-                    data={chartMovingAverage}
-                  />
-                </VictoryChart>
+          <Grid container direction='row' spacing={3}>                        
+            <Grid item xs={12} lg={8}>
+              <Card >
+                <CardContent>
+                  {/* <ResponsiveContainer width="100%" height="100%"> */}
+                    <LineChart
+                      width={500}
+                      height={300}
+                      data={data}
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                      }}
+                    >          
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <Tooltip />
+                      <Legend />
+                      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                  {/* </ResponsiveContainer> */}
+                </CardContent>
+              </Card>
+              
+            </Grid>           
+
+            <Grid item xs={12} lg={4}>
+              {/* <Card >
+                <CardContent> */}
+                  <VictoryChart
+                    theme={VictoryTheme.material}
+                    domainPadding={{ x: 15 }}
+                    height={250}
+                  >
+                    <VictoryLine
+                      data={chartMovingAverage}
+                    />
+                 </VictoryChart>
+                {/* </CardContent>
+              </Card> */}
             </Grid>
           </Grid>
         </Grid>
 
         <Grid item>
-          <Grid container direction='row' spacing={4}>
+          <Grid container direction='row' spacing={3}>
             <Grid item xs={12} lg={6}>
-              <Typography variant="h1" component="div" gutterBottom>
-                {valuePeriod}M
-              </Typography>
+              <Card >
+                <CardContent>
+                  <Typography variant="h1" component="div" gutterBottom>
+                    {valuePeriod}M
+                  </Typography>              
+                </CardContent>
+              </Card>
             </Grid>
             <Grid item xs={12} lg={6}>
-              <Typography variant="h1" component="div" gutterBottom>
-                {valueWay}%
-              </Typography>
+              <Card >
+                <CardContent>
+                  <Typography variant="h1" component="div" gutterBottom>
+                    {valueWay}%
+                  </Typography>
+                </CardContent>
+              </Card>              
             </Grid>
           </Grid>
         </Grid>
