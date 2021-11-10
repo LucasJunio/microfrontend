8545959f87a12135ee2118e2da72a7a92253275a
Carlos Santos <carlos.santos@vileve.com.br>
2021-11-10 12:41:14 -0300

    build: :construction_worker: update build

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,12 +2,11 @@ import axios from "axios";
 // const url = "http://3.233.0.255:3001/api/v1";
 
 const api = axios.create({
-  baseURL: "https://vileveway-backend-lb-develop.vileveway.com.br/api/v1",
+  baseURL: process.env.REACT_APP_END_POINT,
 });
 
 const apiRecovery = axios.create({
-  baseURL:
-    "https://apirecoverypassword-backend-lb-develop.vileveway.com.br/api/v1",
+  baseURL: process.env.REACT_APP_RECOVERY,
 });
 
 export { api, apiRecovery };
